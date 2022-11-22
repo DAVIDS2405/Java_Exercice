665f55d840b6d623a59ccbf070c9b6acd8a1abb7
Nestor <nestor.chumania@epn.edu.ec>
2022-11-22 11:47:52 -0500

    Prueba

--- src/Cajero.java
+++ src/Cajero.java
@@ -1,31 +1,35 @@
 public class Cajero {
     //ATTRIBUTES
-    boolean Tarjeta_credito;
     String Persona_titular_cuenta;
-    int contraseña;
-    int Dinero_cuenta;
+    double Dinero_cuenta;
     
 
     //CONSTRUCTOR
-    public Cajero(String persona_titular_cuenta, int dinero_cuenta) {
-        Persona_titular_cuenta = persona_titular_cuenta;
-        Dinero_cuenta = dinero_cuenta;
+    
+    
+
+    public void Cajero(){
+        Persona_titular_cuenta = "";
+        Dinero_cuenta = 0;
     }
-
-
+        
+    
     
     // METHODS
-    public void Depositar_dinero(int dinero){
+    public void Depositar_dinero(double dinero){
         System.out.println("deposito");
         if ( dinero < 0 || dinero == 0 || getDinero_cuenta() == 0 ){
             System.out.println("ingrese dinero valido");
+            
         }
         else{
             setDinero_cuenta(getDinero_cuenta()- dinero);
         }
     }
 
-    public void Retirar_dinero(int dinero){
+    
+
+    public void Retirar_dinero(double dinero){
         if ( dinero < 0 || dinero == 0 || getDinero_cuenta() == 0 ){
             System.out.println("ingrese dinero valido");
         }
@@ -33,8 +37,8 @@ public class Cajero {
             setDinero_cuenta(getDinero_cuenta()- dinero);
         }
     }
-    public void Ingresar_dinero(int dinero){
-        if ( dinero < 0 || dinero == 0 || getDinero_cuenta() == 0 ){
+    public void Ingresar_dinero(double dinero){
+        if ( dinero < 0 || dinero == 0){
             System.out.println("ingrese dinero valido");
         }
         else{
@@ -55,38 +59,20 @@ public class Cajero {
     }
 
 
-    public int getDinero_cuenta() {
+    public double getDinero_cuenta() {
         return Dinero_cuenta;
     }
 
 
-    public void setDinero_cuenta(int dinero_cuenta) {
+    public void setDinero_cuenta(Double dinero_cuenta) {
         Dinero_cuenta = dinero_cuenta;
     }
 
 
-
-    public boolean isTarjeta_credito() {
-        return Tarjeta_credito;
+    @Override
+    public String toString() {
+        return "Cajero [Persona_titular_cuenta=" + Persona_titular_cuenta + ", Dinero_cuenta=" + Dinero_cuenta + "]";
     }
-
-
-
-    public void setTarjeta_credito(boolean tarjeta_credito) {
-        Tarjeta_credito = tarjeta_credito;
-    }
-
-
-
-    public int getContraseña() {
-        return contraseña;
-    }
-
-
-
-    public void setContraseña(int contraseña) {
-        this.contraseña = contraseña;
-    }   
 
     
 
--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,87 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
+import java.util.Scanner;
 
+public class Main {
+    static Scanner sn = new Scanner(System.in);
+    static Cajero Persona1 = new Cajero();
+    public static void main(String[] args) throws Exception {
+        
         int opcion_Menuprincipal = menu_principal();
-        int opcion_Menusecundario = menu_secundario();
+        int contrasenia;
+        String Titular;
+        double dinero;
+        String depositar;
         while(opcion_Menuprincipal != 2){
 
             if (opcion_Menuprincipal == 1){
-               
 
-                while (opcion_Menusecundario !=4){
-                    if(opcion_Menusecundario == 1){
+                sn.nextLine();
+                System.out.println("ingrese el nombre del titular de la cuenta");
+                Titular = sn.nextLine();
+                System.out.println("Cuanto dinero tiene ");
+                dinero = sn.nextDouble();
+                Persona1.setDinero_cuenta(dinero);
+                Persona1.setPersona_titular_cuenta(Titular);
+                System.out.println("");
+                int opcion_Menusecundario = menu_secundario();
+                while (opcion_Menusecundario != 5){
+                    if (opcion_Menusecundario == 1){
+                        
+                        System.out.print("ingrese la contraseña de su tarjeta");
+                        contrasenia = sn.nextInt();
+                        if (contrasenia == 1234 ){
+                            sn.nextLine();
+                            System.out.println("");
+                            System.out.println("ingrese el nombre de la persona a quien se depositara");
+                            depositar = sn.nextLine();
+                            System.out.print("Cuanto dinero desea depositar: ");
+                            dinero = sn.nextDouble();
+                            Persona1.Depositar_dinero(dinero);
+                            System.out.println("se deposito " + dinero+ "$ a " + depositar );
+                        }
+                        else if (contrasenia != 1234){
+                            System.out.println(" no es su contraseña la correcta");
+                        }
+                       
 
                     }
-                    else if ( opcion_Menusecundario == 2){
+                    else if (opcion_Menusecundario == 2){
+                        System.out.print("ingrese la contraseña de su tarjeta");
+                        contrasenia = sn.nextInt();
+                        if (contrasenia == 1234 ){
+                            System.out.println("cuanto dinero desea retirar");
+                            dinero = sn.nextDouble();
+                            Persona1.Retirar_dinero(dinero);
 
+                        }
+                        else{
+                            System.out.println(" no es su contraseña la correcta");
+                        }
+                        
                     }
                     else if (opcion_Menusecundario == 3){
+                        System.out.print("ingrese la contraseña de su tarjeta");
+                        contrasenia = sn.nextInt();
+                        if (contrasenia == 1234 ){
+                            System.out.println("Cuanto dinero desea ingresar");
+                            dinero = sn.nextDouble();
+                            Persona1.Ingresar_dinero(dinero);
+                        }
+                        else{
+                            System.out.println(" no es su contraseña la correcta");
+                        }
+                        
 
                     }
-                    else if ( opcion_Menusecundario ==4 ){
-                        System.exit(0);
+                    else if (opcion_Menusecundario == 4){
+                        System.out.println(Persona1.getDinero_cuenta());
+                        System.out.println(Persona1.getPersona_titular_cuenta());
                     }
-
 
                     opcion_Menusecundario = menu_secundario();
                 }
                 
             }
-            else{
-                System.exit(0);
-            }
+           
 
             opcion_Menuprincipal = menu_principal();
 
@@ -48,8 +100,9 @@ public class Main {
             System.out.println("Bancho Pichincha");
             System.out.println("1.- Ingresar tarjeta");
             System.out.println("2.- Salir");
-
-            if (opcion >2 || opcion < 1){
+            System.out.print("ingrese una opcion ");
+            opcion = sn.nextInt();
+            if (opcion > 2 || opcion < 1){
                 System.out.println("ingrese una opcion valida entre 1 y 2");
             }
         }
@@ -60,14 +113,17 @@ public class Main {
     //Menu Secundario
     public static int menu_secundario(){
         int opcion = 0;
-        while(opcion > 4 || opcion < 1){
+        while(opcion > 5 || opcion < 1){
             System.out.println("Bancho Pichincha");
-            System.out.println("1.- Ingresar dinero");
-            System.out.println("2.- Depósitar dinero");
-            System.out.println("3.- Retirar dinero");
-            System.out.println("4.- Salir");
+            System.out.println("1.- Depósitar dinero");
+            System.out.println("2.- Retirar dinero");
+            System.out.println("3.- Ingresar dinero");
+            System.out.println("4.- Ver dinero en la cuenta");
+            System.out.println("5.- Salir");
+            System.out.print("ingrese una opcion ");
+            opcion = sn.nextInt();
 
-            if (opcion >4 || opcion < 1){
+            if (opcion >5 || opcion < 1){
                 System.out.println("ingrese una opcion valida entre 1 y 4");
             }
         }
