6e9dcb1b239ab665670dee8696612e47564c712f
David Basantes <sebastian2405lucero@hotmail.com>
2023-01-23 09:35:04 -0500

    calculadora

--- src/App.java
+++ src/App.java
@@ -1,6 +1,25 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Adios");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+
+public class App extends Application {
+    public static void main(String[] args) {
+      launch(args);
     }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+      Parent calculadora = FXMLLoader.load(getClass().getResource("/assets/view/Calculator.fxml"));
+      Scene scene = new Scene(calculadora);
+      scene.setFill(Color.TRANSPARENT);
+      stage.setScene(scene);
+     
+      stage.show();
+    }
+    
+    
 }
---
+++ src/controller/Calculator.java
@@ -0,0 +1,38 @@
+package controller;
+
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Calculator {
+
+    @FXML
+    private Button ClearAll;
+
+    @FXML
+    private Button ClearNumber;
+
+    @FXML
+    private Button Clearone;
+
+
+    @FXML
+    private TextField Result;
+
+
+    @FXML
+    private Label Prevnum;
+    
+    @FXML
+    private TextArea History_Register;
+    
+
+    
+    
+    
+}
+
